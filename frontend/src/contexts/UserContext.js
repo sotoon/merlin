616e8d5a02e1d4f4c9865589ed3fe902bdc04229
Amin Salarkia <mohammadamin.salarkia@sotoon.ir>
2024-01-08 18:45:20 +0330

    dashboard page

---
+++ frontend/src/contexts/UserContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from "react";
+import PropTypes from "prop-types";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+UserProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
