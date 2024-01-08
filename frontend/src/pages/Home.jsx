5dfdd994370fac63454d28bf3b0d7be3ebbac12e
Amin Salarkia <mohammadamin.salarkia@sotoon.ir>
2024-01-08 15:30:42 +0330

    add linter and formatter to react app

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Home = () => {
-    return (
-        <div />
-    )
-}
+  return <div />;
+};
 
 export default Home;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
-API.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
