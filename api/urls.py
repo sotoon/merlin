5de52c2f25c91ebd897820599052dc3d56e2f66f
Amin Salarkia <mohammadamin.salarkia@sotoon.ir>
2024-01-08 17:02:46 +0330

    handle refresh token

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from api.views import BepaCallbackView, LoginView, SignupView
 
@@ -6,4 +7,5 @@ urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("bepa-callback/", BepaCallbackView.as_view(), name="bepa-callback"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
