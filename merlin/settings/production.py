aeead12c3c549a08400972bdc5e2eda2dd98988a
Amin Salarkia <mohammadamin.salarkia@sotoon.ir>
2024-01-17 14:17:04 +0330

    separate dev and prod settings

---
+++ merlin/settings/production.py
@@ -0,0 +1,17 @@
+from merlin.settings.base import *
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "merlin",
+        "USER": "merlin",
+        "PASSWORD": os.environ.get("MERLIN_DATABASE_PASSWORD", ""),
+        "HOST": os.environ.get("MERLIN_DATABASE_HOST", ""),
+        "PORT": "5432",
+        "OPTIONS": {
+            "connect_timeout": 60,
+        },
+    },
+}
