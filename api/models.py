a4e11bd1f1e952fcde72e12750086a9a8df1fa5f
Amin Salarkia <mohammadamin.salarkia@sotoon.ir>
2024-01-24 17:37:34 +0330

    define Department, Chapter, and Team. Complete Admin Page

--- api/models.py
+++ api/models.py
@@ -2,27 +2,70 @@ import uuid
 
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 
 class MerlinBaseModel(models.Model):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     date_created = models.DateTimeField(
-        _("Date Created"), null=True, blank=True, auto_now_add=True
+        "تاریخ ساخت", null=True, blank=True, auto_now_add=True
     )
 
     date_updated = models.DateTimeField(
-        _("Date Updated"), null=True, blank=True, auto_now=True
+        "تاریخ بروزرسانی", null=True, blank=True, auto_now=True
     )
 
     class Meta:
         abstract = True
 
 
+class Department(MerlinBaseModel):
+    name = models.CharField(max_length=256, verbose_name="نام")
+    description = models.TextField(verbose_name="توضیحات")
+
+    class Meta:
+        verbose_name = "دپارتمان"
+        verbose_name_plural = "دپارتمان‌ها"
+
+    def __str__(self):
+        return self.name
+
+
+class Chapter(MerlinBaseModel):
+    name = models.CharField(max_length=256, verbose_name="نام")
+    department = models.ForeignKey(
+        Department, on_delete=models.SET_NULL, null=True, verbose_name="دپارتمان"
+    )
+    leader = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="لیدر")
+    description = models.TextField(verbose_name="توضیحات")
+
+    class Meta:
+        verbose_name = "چپتر"
+        verbose_name_plural = "چپترها"
+
+    def __str__(self):
+        return self.name
+
+
+class Team(MerlinBaseModel):
+    name = models.CharField(max_length=256, verbose_name="نام")
+    department = models.ForeignKey(
+        Department, on_delete=models.SET_NULL, null=True, verbose_name="دپارتمان"
+    )
+    leader = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="لیدر")
+    description = models.TextField(verbose_name="توضیحات")
+
+    class Meta:
+        verbose_name = "تیم"
+        verbose_name_plural = "تیم‌ها"
+
+    def __str__(self):
+        return self.name
+
+
 class NoteType(models.TextChoices):
-    GOAL = "Goal"
-    MEETING = "Meeting"
-    Personal = "Personal"
+    GOAL = "Goal", "هدف"
+    MEETING = "Meeting", "جلسه"
+    Personal = "Personal", "شخصی"
 
     @classmethod
     def default(cls):
@@ -30,10 +73,20 @@ class NoteType(models.TextChoices):
 
 
 class Note(MerlinBaseModel):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=512)
-    content = models.TextField()
-    date = models.DateField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="نویسنده")
+    title = models.CharField(max_length=512, verbose_name="عنوان")
+    content = models.TextField(verbose_name="محتوا")
+    date = models.DateField(verbose_name="تاریخ")
     type = models.CharField(
-        max_length=128, choices=NoteType.choices, default=NoteType.default()
+        max_length=128,
+        choices=NoteType.choices,
+        default=NoteType.default(),
+        verbose_name="نوع",
     )
+
+    class Meta:
+        verbose_name = "یادداشت"
+        verbose_name_plural = "یادداشت‌ها"
+
+    def __str__(self):
+        return self.title
