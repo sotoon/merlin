3c768ae022800ac1291080074c7e607c34a9de9f
Hassan Mojab <mh_mojab75@yahoo.com>
2024-02-27 18:10:47 +0330

    chore: setup i18n

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/eslint-module'],
+  modules: ['@nuxtjs/eslint-module', '@nuxtjs/i18n'],
   srcDir: 'src/',
   ssr: false,
 
   devtools: { enabled: true },
+
+  i18n: {
+    locales: [{ code: 'fa', iso: 'fa-IR', dir: 'rtl', file: 'fa.json' }],
+    langDir: './locales',
+    defaultLocale: 'fa',
+    strategy: 'no_prefix',
+  },
 
   typescript: {
     shim: false,
