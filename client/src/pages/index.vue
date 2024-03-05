2eece81625d0ce7c9fa0f7141514d215f3c06961
Hassan Mojab <mh_mojab75@yahoo.com>
2024-03-05 20:18:20 +0330

    feat: add login and profile api

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -3,13 +3,25 @@
     <p class="text-center text-lg text-primary">
       {{ t('common.appName') }}
     </p>
+
+    <div class="flex flex-col items-center">
+      <PLoading v-if="pending" />
+      <p v-else class="text-center" dir="ltr">Welcome {{ data?.name }}!</p>
+      <PButton class="my-4" @click="logout">Logout</PButton>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { PButton, PLoading } from '@pey/core';
+
 definePageMeta({
   middleware: ['auth'],
 });
 
 const { t } = useI18n();
+
+const logout = useLogout();
+
+const { data, pending } = useGetProfile();
 </script>
