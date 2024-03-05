d70f0bda87bd59fd0e1cd8e2577eee5612031099
Hassan Mojab <mh_mojab75@yahoo.com>
2024-03-05 20:18:02 +0330

    feat: add route middlewares to handle auth redirections

---
+++ client/src/middleware/auth.ts
@@ -0,0 +1,7 @@
+export default defineNuxtRouteMiddleware(() => {
+  const { $authStore } = useNuxtApp();
+
+  if (!$authStore.tokens.refresh) {
+    return navigateTo('/login');
+  }
+});
--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -7,5 +7,9 @@
 </template>
 
 <script lang="ts" setup>
+definePageMeta({
+  middleware: ['auth'],
+});
+
 const { t } = useI18n();
 </script>
