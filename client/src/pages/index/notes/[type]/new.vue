0b861e65b4909ed4d52ed04f2a0102ea40f49973
Hassan Mojab <mh_mojab75@yahoo.com>
2024-03-26 00:33:47 +0330

    feat: implement note detail page

---
+++ client/src/pages/index/notes/[type]/new.vue
@@ -0,0 +1,9 @@
+<template>
+  <PBox class="bg-white px-4 py-8 lg:px-8 lg:pt-10">
+    <NoteDetail />
+  </PBox>
+</template>
+
+<script lang="ts" setup>
+import { PBox } from '@pey/core';
+</script>
