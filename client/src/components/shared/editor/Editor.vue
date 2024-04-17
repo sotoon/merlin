876cf95ef73c7a64a887188698c651574aeb006b
Hassan Mojab <mh_mojab75@yahoo.com>
2024-04-17 15:26:22 +0330

    fix: replace sanitize-html with tiptap editor content

--- client/src/components/shared/editor/Editor.vue
+++ client/src/components/shared/editor/Editor.vue
@@ -5,7 +5,7 @@
     <EditorToolbar :editor="editor" />
 
     <div
-      class="prose max-w-none bg-white px-4 py-2 text-gray-100 transition [&_*]:outline-none"
+      class="text-initial prose max-w-none bg-white px-4 py-2 text-gray-100 transition [&_*]:outline-none"
     >
       <TiptapEditorContent :editor="editor" />
     </div>
@@ -15,34 +15,13 @@
 </template>
 
 <script lang="ts" setup>
-import { Link as TiptapLink } from '@tiptap/extension-link';
-import { Underline as TiptapUnderline } from '@tiptap/extension-underline';
-import TextDirection from 'tiptap-text-direction';
+import { useCustomEditor } from './useCustomEditor';
 
 const props = defineProps<{ modelValue: string }>();
 const emit = defineEmits<{ 'update:model-value': [value: string] }>();
 
-const editor = useEditor({
+const editor = useCustomEditor({
   content: props.modelValue,
-  extensions: [
-    TiptapStarterKit,
-    TiptapUnderline,
-    TiptapLink.extend({
-      inclusive: false,
-    }).configure({
-      openOnClick: false,
-    }),
-    TextDirection.configure({
-      types: [
-        'heading',
-        'paragraph',
-        'blockquote',
-        'bulletList',
-        'orderedList',
-        'codeBlock',
-      ],
-    }),
-  ],
   onUpdate: ({ editor }) => {
     emit('update:model-value', editor.getHTML());
   },
@@ -56,8 +35,4 @@ watch(
     }
   },
 );
-
-onBeforeUnmount(() => {
-  editor.value?.destroy();
-});
 </script>
