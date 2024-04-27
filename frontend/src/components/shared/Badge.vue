d9d0abc021cf60fc26fa4671246f39afc1ead0d4
Hassan Mojab <mh_mojab75@yahoo.com>
2024-04-27 19:45:54 +0330

    feat: implement badge for new messages count in sidebar

---
+++ frontend/src/components/shared/Badge.vue
@@ -0,0 +1,22 @@
+<template>
+  <Transition
+    class="transition-transform"
+    enter-active-class="scale-0"
+    leave-active-class="scale-0"
+  >
+    <PText
+      v-if="count"
+      class="absolute left-4 block rounded bg-danger px-2 pt-1 text-center text-white"
+      variant="caption2"
+      weight="medium"
+    >
+      {{ count.toLocaleString('fa-IR') }}
+    </PText>
+  </Transition>
+</template>
+
+<script lang="ts" setup>
+import { PText } from '@pey/core';
+
+defineProps<{ count?: number }>();
+</script>
