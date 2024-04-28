97d0df65dc6f8887b439daa7d3feef238ca8f0db
Hassan Mojab <mh_mojab75@yahoo.com>
2024-04-28 22:37:44 +0330

    feat: implement table feature for editor

--- frontend/src/components/shared/editor/useCustomEditor.ts
+++ frontend/src/components/shared/editor/useCustomEditor.ts
@@ -1,7 +1,12 @@
-import { Link as TiptapLink } from '@tiptap/extension-link';
-import { Underline as TiptapUnderline } from '@tiptap/extension-underline';
-import TextDirection from 'tiptap-text-direction';
 import type { EditorOptions } from '@tiptap/core';
+
+import { Link as TiptapLink } from '@tiptap/extension-link';
+import { Table as TiptapTable } from '@tiptap/extension-table';
+import { TableCell as TiptapTableCell } from '@tiptap/extension-table-cell';
+import { TableHeader as TiptapTableHeader } from '@tiptap/extension-table-header';
+import { TableRow as TiptapTableRow } from '@tiptap/extension-table-row';
+import { Underline as TiptapUnderline } from '@tiptap/extension-underline';
+import { TextDirection as TiptapTextDirection } from 'tiptap-text-direction';
 
 export const useCustomEditor = (options: Partial<EditorOptions> = {}) => {
   const editor = useEditor({
@@ -14,7 +19,13 @@ export const useCustomEditor = (options: Partial<EditorOptions> = {}) => {
       }).configure({
         openOnClick: 'whenNotEditable',
       }),
-      TextDirection.configure({
+      TiptapTable.configure({
+        resizable: true,
+      }),
+      TiptapTableRow,
+      TiptapTableHeader,
+      TiptapTableCell,
+      TiptapTextDirection.configure({
         types: [
           'heading',
           'paragraph',
