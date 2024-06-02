94b4274bd036121e287e5a329cc435968f68ada5
Hassan Mojab <mh_mojab75@yahoo.com>
2024-06-02 20:49:17 +0330

    refactor: improve NoteDetails component structure

---
+++ frontend/src/components/note/NoteLinkChip.vue
@@ -0,0 +1,40 @@
+<template>
+  <NuxtLink v-if="note" class="*:cursor-pointer" :to="noteLinkTo">
+    <PChip
+      color="primary"
+      :icon="PeyLinkIcon"
+      :label="note.title"
+      size="small"
+    />
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+import { PChip } from '@pey/core';
+import { PeyLinkIcon } from '@pey/icons';
+
+const props = defineProps<{ noteId: string }>();
+
+const { data: note } = useGetNote({ id: props.noteId });
+
+const noteLinkTo = computed(() => {
+  if (!note.value) {
+    return undefined;
+  }
+
+  if (note.value.type === NOTE_TYPE.template) {
+    return {
+      name: 'template',
+      params: { id: note.value.uuid },
+    };
+  }
+
+  return {
+    name: 'note',
+    params: {
+      type: NOTE_TYPE_ROUTE_PARAM[note.value.type],
+      id: note.value.uuid,
+    },
+  };
+});
+</script>
