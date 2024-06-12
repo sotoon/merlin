5a8a0f43272145fc7de5376ed5889e2b1d9ac8f8
Hassan Mojab <mh_mojab75@yahoo.com>
2024-06-12 16:27:58 +0330

    feat: add delete for note feedbacks

---
+++ frontend/src/components/note/NoteFeedbackCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <PCard header-variant="primary" :title="feedback.owner_name">
+    <article>
+      <EditorContent :content="feedback.content" />
+    </article>
+
+    <template v-if="feedback.owner === profile?.email" #toolbar>
+      <div class="flex items-center justify-end gap-3 sm:ms-4">
+        <div class="flex h-8 w-8 items-center justify-center">
+          <PLoading v-if="isDeleteLoading" class="text-primary" />
+
+          <PInlineConfirm
+            v-else
+            :confirm-button-text="t('common.delete')"
+            :message="t('note.confirmDeleteNoteFeedback')"
+            @confirm="deleteNoteFeedback"
+          >
+            <PIconButton color="danger" :icon="PeyTrashIcon" variant="ghost" />
+          </PInlineConfirm>
+        </div>
+
+        <PIconButton
+          :icon="PeyEditIcon"
+          variant="ghost"
+          @click="emit('edit')"
+        />
+      </div>
+    </template>
+  </PCard>
+</template>
+
+<script lang="ts" setup>
+import { PCard, PIconButton, PInlineConfirm, PLoading } from '@pey/core';
+import { PeyEditIcon, PeyTrashIcon } from '@pey/icons';
+
+const props = defineProps<{ note: Note; feedback: NoteFeedback }>();
+const emit = defineEmits<{ edit: [] }>();
+
+const { t } = useI18n();
+const { data: profile } = useGetProfile();
+const { execute: deleteNoteFeedback, pending: isDeleteLoading } =
+  useDeleteNoteFeedback({
+    noteId: props.note.uuid,
+    feedbackId: props.feedback.uuid,
+  });
+</script>
--- frontend/src/components/note/NoteFeedbackList.vue
+++ frontend/src/components/note/NoteFeedbackList.vue
@@ -2,40 +2,24 @@
   <ul class="grid grid-cols-1 gap-2 py-4 lg:gap-3">
     <li v-for="feedback in feedbacks" :key="feedback.uuid">
       <NoteFeedbackForm
-        v-model:visible="showForm"
         :note="note"
         :feedback="feedback"
+        :visible="showFormId === feedback.uuid"
+        @update:visible="showFormId = null"
       />
 
-      <PCard
-        v-if="!showForm"
-        header-variant="primary"
-        :title="feedback.owner_name"
-      >
-        <article>
-          <EditorContent :content="feedback.content" />
-        </article>
-
-        <template #toolbar>
-          <PIconButton
-            v-if="feedback.owner === profile?.email"
-            :icon="PeyEditIcon"
-            variant="ghost"
-            @click="showForm = true"
-          />
-        </template>
-      </PCard>
+      <NoteFeedbackCard
+        v-if="showFormId !== feedback.uuid"
+        :note="note"
+        :feedback="feedback"
+        @edit="showFormId = feedback.uuid"
+      />
     </li>
   </ul>
 </template>
 
 <script lang="ts" setup>
-import { PCard, PIconButton } from '@pey/core';
-import { PeyEditIcon } from '@pey/icons';
-
 defineProps<{ note: Note; feedbacks: NoteFeedback[] }>();
 
-const showForm = ref(false);
-
-const { data: profile } = useGetProfile();
+const showFormId = ref<string | null>(null);
 </script>
