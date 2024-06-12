d5b0cd0a0f9769fe9a147a5680058b05b43bcaa3
Hassan Mojab <mh_mojab75@yahoo.com>
2024-06-12 16:53:38 +0330

    refactor: group note components

---
+++ frontend/src/components/note/list/NoteList.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="mt-4 grid grid-cols-1 gap-2 py-4 xl:grid-cols-2 xl:gap-3">
+    <li v-for="note in notes" :key="note.uuid">
+      <NuxtLink :to="getNoteLink(note)">
+        <NoteCard
+          :note="note"
+          :display-writer="displayWriter"
+          :display-type="displayType"
+          :display-read-status="displayReadStatus"
+        />
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  notes: Note[];
+  displayWriter?: boolean;
+  displayType?: boolean;
+  displayReadStatus?: boolean;
+}>();
+
+const route = useRoute();
+
+const getNoteLink = (note: Note) => {
+  if (note.type === NOTE_TYPE.template) {
+    return {
+      name: 'template',
+      params: {
+        id: note.uuid,
+      },
+    };
+  }
+
+  const read =
+    props.displayReadStatus && !note.read_status ? 'true' : undefined;
+
+  return {
+    name: 'note',
+    params: {
+      type: route.params.type || '-',
+      id: note.uuid,
+    },
+    query: { read },
+  };
+};
+</script>
