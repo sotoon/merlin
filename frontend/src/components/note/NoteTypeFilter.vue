681489824fc649d147842719c2d01ce5f763014c
Hassan Mojab <mh_mojab75@yahoo.com>
2024-07-03 16:43:28 +0330

    feat: use tabs for note type filter

--- frontend/src/components/note/NoteTypeFilter.vue
+++ frontend/src/components/note/NoteTypeFilter.vue
@@ -1,37 +1,33 @@
 <template>
-  <div class="w-28">
-    <PListbox
-      :model-value="typeFilter"
-      clearable
-      hide-details
-      :placeholder="t('note.type')"
-      size="small"
-      @update:model-value="
-        (value) => (typeFilter = value === '' ? undefined : value)
-      "
-    >
-      <PListboxOption
-        v-for="{ label, value } in typeOptions"
-        :key="value"
-        :label="label"
-        :value="value"
-      />
-    </PListbox>
-  </div>
+  <PTabs v-model="selectedTab" class="rounded bg-white" variant="box">
+    <PTab v-for="{ label, value } in typeOptions" :key="value" :title="label" />
+  </PTabs>
 </template>
 
 <script lang="ts" setup>
-import { PListbox, PListboxOption } from '@pey/core';
+import { PTab, PTabs } from '@pey/core';
 import { useRouteQuery } from '@vueuse/router';
 
 const { t } = useI18n();
-const typeFilter = useRouteQuery<string>('type', undefined);
+const typeFilter = useRouteQuery<string | undefined>('type', undefined);
 
 const typeOptions = computed(() => [
+  { label: t('common.all') },
   { label: t('noteType.goal'), value: NOTE_TYPE.goal },
   { label: t('noteType.task'), value: NOTE_TYPE.task },
   { label: t('noteType.meeting'), value: NOTE_TYPE.meeting },
   { label: t('noteType.proposal'), value: NOTE_TYPE.proposal },
   { label: t('noteType.message'), value: NOTE_TYPE.message },
 ]);
+const selectedTab = computed({
+  get() {
+    return (
+      typeOptions.value.findIndex((type) => type.value === typeFilter.value) ??
+      0
+    );
+  },
+  set(newValue) {
+    typeFilter.value = typeOptions.value[newValue]?.value ?? undefined;
+  },
+});
 </script>
