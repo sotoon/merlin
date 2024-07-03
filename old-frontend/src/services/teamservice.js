e2239ab30c6687702678644dd39dc7184e4e8134
Hassan Mojab <mh_mojab75@yahoo.com>
2024-07-03 10:23:24 +0330

    chore: remove old-frontend

--- old-frontend/src/services/teamservice.js
+++
@@ -1,9 +0,0 @@
-import { apiCall } from "./api";
-
-export const getMyTeam = async () => {
-  return await apiCall("get", "/my-team/");
-};
-
-export const getAllUsers = async () => {
-  return await apiCall("get", "/users/");
-};
