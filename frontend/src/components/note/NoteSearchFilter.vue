0667b5dd1ba643b48661e0493de5080923385408
Hassan Mojab <mh_mojab75@yahoo.com>
2024-07-10 16:33:23 +0330

    feat: implement note search filter

---
+++ frontend/src/components/note/NoteSearchFilter.vue
@@ -0,0 +1,22 @@
+<template>
+  <PInput
+    v-model="query"
+    hide-details
+    :placeholder="t('common.search')"
+    size="small"
+    type="search"
+  >
+    <template #iconStart>
+      <PeySearchIcon class="text-gray-50" :size="20" />
+    </template>
+  </PInput>
+</template>
+
+<script lang="ts" setup>
+import { PInput } from '@pey/core';
+import { PeySearchIcon } from '@pey/icons';
+import { useRouteQuery } from '@vueuse/router';
+
+const { t } = useI18n();
+const query = useRouteQuery('q', '');
+</script>
--- frontend/src/composables/useFilterNotes.ts
+++ frontend/src/composables/useFilterNotes.ts
@@ -1,7 +1,16 @@
 import { useRouteQuery } from '@vueuse/router';
 
+const NOTE_SEARCHABLE_KEYS = [
+  'content',
+  'owner',
+  'owner_name',
+  'title',
+] satisfies (keyof Note)[];
+
 export const useFilterNotes = (notes: Ref<Note[]> | (() => Note[])) => {
   const { data: myTeam } = useGetMyTeam();
+
+  const queryFilter = useRouteQuery<string>('q');
   const typeFilter = useRouteQuery<string>('type');
   const writerFilter = useRouteQuery<string>('writer');
   const yearFilter = useRouteQuery('year', undefined, {
@@ -16,6 +25,10 @@ export const useFilterNotes = (notes: Ref<Note[]> | (() => Note[])) => {
   const filteredNotes = computed(() =>
     toValue(notes).filter(
       (note) =>
+        (!queryFilter.value ||
+          NOTE_SEARCHABLE_KEYS.some((key) =>
+            note[key]?.toLowerCase()?.includes(queryFilter.value.toLowerCase()),
+          )) &&
         note.type === (typeFilter.value ?? note.type) &&
         note.owner === (writerFilter.value ?? note.owner) &&
         note.year === (yearFilter.value ?? note.year) &&
