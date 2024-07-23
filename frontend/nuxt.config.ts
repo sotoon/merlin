937ca75d8bffadf5cd6988c6d90e38c7189cd736
Hassan Mojab <mh_mojab75@yahoo.com>
2024-07-23 16:38:41 +0330

    feat: add new favicons and webmanifest

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,6 +5,27 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        {
+          rel: 'manifest',
+          href: '/site.webmanifest',
+        },
+        {
           rel: 'stylesheet',
           type: 'text/css',
           href: 'https://fonts.web.sotoon.ir/IRANYekan/fonts.css',
