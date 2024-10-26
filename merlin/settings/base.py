269bd9b153ebae7bd46d33c5ad6d1df9a1fdadc0
Alireza Asadi <alireza.asadi@cafebazaar.ir>
2024-10-26 18:07:43 +0330

    Revert ACCESS_TOKEN_LIFETIME

--- merlin/settings/base.py
+++ merlin/settings/base.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
