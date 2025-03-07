54124aee1ec404f484b30aa91ba0fc166d12e13e
alirezaAsadi2018 <asady529@gmail.com>
2025-03-08 02:20:39 +0330

    Fix linting problems

--- api/models.py
+++ api/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 import logging
-from typing import Union
 
 logger = logging.getLogger(__name__)
 
