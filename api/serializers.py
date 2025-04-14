93ba75a4aab05f2bf617d34259e74e1d22570802
alirezaAsadi2018 <asady529@gmail.com>
2025-04-14 12:04:43 +0330

    Remove summary to_representation

--- api/serializers.py
+++ api/serializers.py
@@ -209,10 +209,6 @@ class SummarySerializer(serializers.ModelSerializer):
         )
         return instance
 
-    def to_representation(self, instance):
-        data = super(NoteSerializer, self).to_representation(instance)
-        data['submit_status'] = data.pop('submit_status_name')
-        return data
 
 class QuestionSerializer(serializers.ModelSerializer):
     """
