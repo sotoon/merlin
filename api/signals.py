ff508b091486f80c49c4e8fa26bb77d5a25d88d0
Ali Amiri <aliamirii.am@gmail.com>
2025-04-23 17:26:47 +0330

    fix: remove files related to email feature

--- api/signals.py
+++ api/signals.py
@@ -1,6 +1,5 @@
 from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
-from django.core.exceptions import ValidationError
 
 from .models import (
     Committee,
