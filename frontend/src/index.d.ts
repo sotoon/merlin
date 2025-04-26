ee2e28e8443791395e8157a7899c06ff374d3d82
iman malekian <imanmalekian31@gmail.com>
2025-04-26 19:07:39 +0330

    refactor: move AuthTokens and AuthStore interfaces into global declaration

--- frontend/src/index.d.ts
+++ frontend/src/index.d.ts
@@ -1,18 +1,16 @@
 import type { components } from '~/types/schema';
 
-declare interface AuthTokens {
-  access: string | null;
-  refresh: string | null;
-}
-
-declare interface AuthStore {
-  readonly tokens: Readonly<AuthTokens>;
-  readonly setTokens: (tokens: Partial<AuthTokens>) => void;
-  readonly removeTokens: () => void;
-}
-
 type AllSchemas = components['schemas'];
 
 declare global {
   export type Schema<T extends keyof AllSchemas> = AllSchemas[T];
+  export interface AuthStore {
+    readonly tokens: Readonly<AuthTokens>;
+    readonly setTokens: (tokens: Partial<AuthTokens>) => void;
+    readonly removeTokens: () => void;
+  }
+  export interface AuthTokens {
+    access: string | null;
+    refresh: string | null;
+  }
 }
