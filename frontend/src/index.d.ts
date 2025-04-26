db149f12c29de0e26e2e399e199ff332dda30154
iman malekian <imanmalekian31@gmail.com>
2025-04-26 18:32:23 +0330

    feat(schema): Add OpenAPI schema for API endpoints and data models

--- frontend/src/index.d.ts
+++ frontend/src/index.d.ts
@@ -1,3 +1,5 @@
+import type { components } from '~/types/schema';
+
 declare interface AuthTokens {
   access: string | null;
   refresh: string | null;
@@ -7,4 +9,10 @@ declare interface AuthStore {
   readonly tokens: Readonly<AuthTokens>;
   readonly setTokens: (tokens: Partial<AuthTokens>) => void;
   readonly removeTokens: () => void;
+}
+
+type AllSchemas = components['schemas'];
+
+declare global {
+  export type Schema<T extends keyof AllSchemas> = AllSchemas[T];
 }
