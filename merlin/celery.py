7877dbe5f6222e0e38e400ae7f6d5770332e131f
Ali Amiri <aliamirii.am@gmail.com>
2025-05-31 17:55:45 +0330

    feat: add basic celery beat

--- merlin/celery.py
+++ merlin/celery.py
@@ -2,17 +2,25 @@ from __future__ import absolute_import, unicode_literals
 import os
 import django
 from celery import Celery
+from celery.schedules import crontab
 
 # Use the DJANGO_SETTINGS_MODULE environment variable if set, otherwise default to development
-# Set the DJANGO_SETTINGS_MODULE in the production environment
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE', 'merlin.settings.development'))
 
 django.setup()
 
 app = Celery('merlin')
-
 app.config_from_object('django.conf:settings', namespace='CELERY') # Means all celery-related config keys should have a `CELERY_` prefix
+
+app.conf.beat_schedule = {
+    # I can add scheduled tasks here in the future.
+    # Example (leave commented for now):
+    # 'send-reminder-every-morning': {
+    #     'task': 'api.email_notifications.tasks.send_pending_request_reminders',
+    #     'schedule': crontab(hour=7, minute=0),
+    # },
+}
 
 app.autodiscover_tasks(['api.email_notifications'])
 
